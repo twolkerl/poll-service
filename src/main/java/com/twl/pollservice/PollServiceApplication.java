1133d552afedd0cf26caa4fd526f352f43b4e7e9
Tiago Wolker <tiago.leite@southsystem.com.br>
2020-03-12 17:28:36 -0300

    Commit inicial

---
+++ src/main/java/com/twl/pollservice/PollServiceApplication.java
@@ -0,0 +1,13 @@
+package com.twl.pollservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PollServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/twl/pollservice/PollServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twl.pollservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
