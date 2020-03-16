6014d7771a0f77280d29386ce64af4a9183fec06
Tiago Wolker <tiago.leite@southsystem.com.br>
2020-03-15 22:41:13 -0300

    Integração com o sistema de verificação de eligibilidade de voto do cpf

--- src/main/java/com/twl/pollservice/service/validator/VoteValidator.java
+++ src/main/java/com/twl/pollservice/service/validator/VoteValidator.java
@@ -1,9 +1,12 @@
 package com.twl.pollservice.service.validator;
 
 import com.twl.pollservice.exception.BusinessException;
+import com.twl.pollservice.integration.UserInfoIntegration;
+import com.twl.pollservice.integration.response.UserInfoResponse;
 import com.twl.pollservice.model.entity.Poll;
 import com.twl.pollservice.model.entity.PollSession;
 import com.twl.pollservice.model.entity.Vote;
+import com.twl.pollservice.model.enums.CpfStatus;
 import com.twl.pollservice.repository.PollSessionRepository;
 import com.twl.pollservice.repository.VoteRepository;
 import org.springframework.stereotype.Component;
@@ -16,27 +19,38 @@ public class VoteValidator {
 
     private final VoteRepository voteRepository;
     private final PollSessionRepository pollSessionRepository;
+    private final UserInfoIntegration userInfoIntegration;
 
-    public VoteValidator(VoteRepository voteRepository, PollSessionRepository pollSessionRepository) {
+    public VoteValidator(VoteRepository voteRepository, PollSessionRepository pollSessionRepository,
+                         UserInfoIntegration userInfoIntegration) {
+
         this.voteRepository = voteRepository;
         this.pollSessionRepository = pollSessionRepository;
+        this.userInfoIntegration = userInfoIntegration;
     }
 
     public void validate(Vote vote) throws Exception {
 
-        Poll poll = vote.getPoll();
+        UserInfoResponse userInfoResponse = userInfoIntegration.checkCpfStatus(vote.getCpf());
 
-        Optional<PollSession> pollSession = pollSessionRepository.findByPollId(poll.getId());
+        if (CpfStatus.UNABLE_TO_VOTE == userInfoResponse.getStatus()) {
+            throw new BusinessException("CPF is unable to vote.");
+        } else {
 
-        if (!pollSession.isPresent()
-                || LocalDateTime.now().isAfter(pollSession.get().getSessionEnd())
-                || LocalDateTime.now().isBefore(pollSession.get().getSessionStart())) {
+            Poll poll = vote.getPoll();
 
-            throw new BusinessException("Voting session closed for given poll.");
-        }
+            Optional<PollSession> pollSession = pollSessionRepository.findByPollId(poll.getId());
 
-        if (voteRepository.findByCpfAndPollId(vote.getCpf(), poll.getId()).isPresent()) {
-            throw new BusinessException("Person already voted in this poll.");
+            if (!pollSession.isPresent()
+                    || LocalDateTime.now().isAfter(pollSession.get().getSessionEnd())
+                    || LocalDateTime.now().isBefore(pollSession.get().getSessionStart())) {
+
+                throw new BusinessException("Voting session closed for given poll.");
+            }
+
+            if (voteRepository.findByCpfAndPollId(vote.getCpf(), poll.getId()).isPresent()) {
+                throw new BusinessException("Person already voted in this poll.");
+            }
         }
     }
 }
